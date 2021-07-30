1e7076370fb4614047c9a97162f719ca1138a90b
sunnythelucky <sunnyparkk@gmail.com>
2021-07-30 13:32:13 -0400

    first commit

---
+++ simpleServer.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+http
+	.createServer(function (req, res) {
+		res.write("Hello, World!");
+		res.end();
+	})
+	.listen(8080);
+
+console.log("Server started! Listening on port 8080");
